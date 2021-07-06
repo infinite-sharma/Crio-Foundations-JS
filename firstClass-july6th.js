6ff0a5c23b3167aada954deb90078971ca039a93
Amit Kumar Sharma <amitksharma1190@gmail.com>
2021-07-06 20:10:32 +0000

    Changed file name for easy reference in future.

--- firstClass-july6th.js
+++ firstClass-july6th.js
@@ -1,21 +1,21 @@
 console.log("Hello world");
 
-console.log(5*5/5);
+console.log(5 * 5 / 5);
 console.log(5 * 15);
 console.log("Hello" + " world");
 console.log(5 + 10 * 5);
-console.log((5+10)*5);
+console.log((5 + 10) * 5);
 
-console.log((5+10)%5);
+console.log((5 + 10) % 5);
 
-var name="amit";
+var name = "amit";
 console.log(name);
 
 let pet = "Narwhal";
 pet = "The Krakan";
 console.log(pet);
 
-var surname= "sharma";
+var surname = "sharma";
 let petAnimal = "cat";
 const isVal = true;
 
@@ -30,27 +30,27 @@ var surname = "Agnihotri";
 surname = ""
 
 
-console.log("the output of 2===2: "+ (2 === 2));
+console.log("the output of 2===2: " + (2 === 2));
 
 let a = 10;
 let b = 20;
 
-console.log( a > b);
-console.log( a === b);
-console.log( a <= b );
-console.log( a != b);
+console.log(a > b);
+console.log(a === b);
+console.log(a <= b);
+console.log(a != b);
 
-function calculateGrade( marks){
+function calculateGrade(marks) {
   let grade = "";
-  if(marks <= 40){
+  if (marks <= 40) {
     grade = "Fail";
-  }else if(marks > 40 && marks <=60){
-    grade = "C" ;
-  }else if(marks > 60 && marks <= 80){
-    grade = "B" ;
-  }else if(marks > 80 && marks <= 100){
+  } else if (marks > 40 && marks <= 60) {
+    grade = "C";
+  } else if (marks > 60 && marks <= 80) {
+    grade = "B";
+  } else if (marks > 80 && marks <= 100) {
     grade = "A";
-  }else{
+  } else {
     grade = "Invalid marks given";
   }
   return grade;
